2766ffb3978c786a50ad50c75970fbe7aa453ba7
unknown <1243702438@qq.com>
2018-05-25 10:24:24 +0800

    1

---
+++ BreakContinue.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DL
+ * Date: 2017/4/18
+ * Time: 上午 10:09
+ */
+
+namespace App\Helpers\DLHttp;
+
+class BreakContinue
+{
+	const AUTO = 1;
+
+	const ON = 2;
+
+	const OFF = 3;
+}
